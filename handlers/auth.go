4a040a20998091e253133c50ab40088880a2b2ef
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-10-27 11:38:11 -0400

    Add Login and CurrentUser functions

--- handlers/auth.go
+++ handlers/auth.go
@@ -2,8 +2,9 @@ package handlers
 
 import (
 	"github.com/carlosm27/jwtGinApi/models"
+	"github.com/carlosm27/jwtGinApi/utils"
 	"github.com/gin-gonic/gin"
-	"log"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -12,25 +13,69 @@ type RegisterInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
-func Register(c *gin.Context) {
+type LoginInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type Server struct {
+	db *gorm.DB
+}
+
+func NewServer(db *gorm.DB) *Server {
+	return &Server{db: db}
+}
+
+func (s *Server) Register(c *gin.Context) {
 	var input RegisterInput
 
 	if err := c.ShouldBind(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+	
 	user := models.User{Username: input.Username, Password: input.Password}
 
-	db, err := models.Setup()
-	if err != nil {
-		log.Println(err)
-	}
-	userRepo := models.RepoInterface(db)
-
-	if _, err = userRepo.CreateUser(&user); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
-		return
+	if err := s.db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "User created"})
+}
+
+func (s *Server) Login(c *gin.Context) {
+	var input LoginInput
+
+	if err := c.ShouldBind(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := models.User{Username: input.Username, Password: input.Password}
+
+	token, err := models.LoginCheck(user.Username, user.Password)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "The username or password is not correct"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func (s *Server) CurrentUser(c *gin.Context) {
+	userId, err := utils.ExtractTokenID(c)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := models.GetUserByID(userId)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Success", "data": user})
 }
