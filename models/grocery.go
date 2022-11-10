74a7ce7162d94111b7931c8acc51a306cb66104e
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-10 17:31:16 -0400

    Create model Grocery

---
+++ models/grocery.go
@@ -0,0 +1,18 @@
+package models
+
+
+import (
+
+    "gorm.io/gorm"
+
+
+)
+
+
+type Grocery struct {
+    gorm.Model
+    Name     string `json: "name"`
+    Quantity int    `json: "quantity"`
+}
+
+
