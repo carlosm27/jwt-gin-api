c931215383028428cd3cfd1d42495a563a9a4052
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-11-10 17:30:43 -0400

    Add Grocery model

---
+++ models/database.go
@@ -0,0 +1,34 @@
+package models
+
+
+
+import (
+	"fmt"
+	
+	//"github.com/joho/godotenv"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func Setup() (*gorm.DB, error) {
+
+	
+	dbUrl := fmt.Sprint(os.Getenv("DATABASE_URL"))
+
+	db, err := gorm.Open(sqlite.Open(dbUrl), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if err = db.AutoMigrate(&User{}); err != nil {
+		log.Println(err)
+	}
+
+	if err = db.AutoMigrate(&Grocery{}); err != nil {
+		log.Println(err)
+	}
+
+	return db, err
+}
